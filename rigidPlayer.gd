a27c6fafb53c186c4c7e9c010ba466d1bb560355
Lukas-12 <lukas.buchsi@gmail.com>
2023-12-15 23:04:00 +0100

    Update rigidPlayer.gd
    Add movement. Need a idea for the jump to check if player is on the floar!

--- rigidPlayer.gd
+++ rigidPlayer.gd
@@ -1,5 +1,5 @@
 extends RigidBody2D
-
+var maxSpeed = 120
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,8 +8,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if(Input.is_action_pressed("down")):
-		linear_velocity.x=1000;
+	print(linear_velocity.y)
+	if(Input.is_action_pressed("ui_right") and linear_velocity.x < maxSpeed):
+		apply_impulse(Vector2(200,0),Vector2(0,0))
 		print("here")
-		
+	if(Input.is_action_pressed("ui_left") and linear_velocity.x > -maxSpeed):
+		apply_impulse(Vector2(-200,0),Vector2(0,0))	
+	if(Input.is_action_pressed("ui_up")and abs(linear_velocity.y)<5):
+		apply_impulse(Vector2(0,-1000),Vector2(0,0))	
 	pass
