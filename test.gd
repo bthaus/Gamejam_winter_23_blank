6e6a80e8ec13c82d161219588f56a9025ec12cf7
bthaus <bwuest@gmx.at>
2023-12-15 21:38:01 +0100

    predangling arm

--- test.gd
+++ test.gd
@@ -9,5 +9,5 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	#constant_linear_velocity.x=-10;
-	position.x=position.x-1
+	
 	pass
