0a0b281b826c533803e05adaff05c3fc66289c85
JohannesLadurner <80148992+JohannesLadurner@users.noreply.github.com>
2023-12-16 16:23:39 +0100

    fixed spider

--- Enemies/spider.gd
+++ Enemies/spider.gd
@@ -39,5 +39,5 @@ func _on_attack_area_body_entered(body):
 	hit = true
 
 func _draw():
-	draw_line(Vector2(0, position.y - global_position.y - 5000), Vector2(0, -50), Color.WHITE, 3)
+	draw_line(Vector2(0, position.y - global_position.y - 5000), Vector2(0, 0), Color.WHITE, 3)
 	
