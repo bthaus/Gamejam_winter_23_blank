fa2d71435b407e21ab77b6fd512926cbf80aa074
bthaus <bwuest@gmx.at>
2023-12-16 10:16:12 +0100

    update sprites puppet

--- rigidPlayer.gd
+++ rigidPlayer.gd
@@ -13,7 +13,7 @@ func _process(delta):
 		apply_impulse(Vector2(200,0),Vector2(0,0))
 	if(Input.is_action_pressed("ui_left") and linear_velocity.x > -maxSpeed):
 		apply_impulse(Vector2(-200,0),Vector2(0,0))	
-	if(Input.is_action_pressed("ui_up")and abs(linear_velocity.y)<5):
+	if(Input.is_action_pressed("ui_up")and abs(linear_velocity.y)<5) and floorcontacts>0:
 		apply_impulse(Vector2(0,-500),Vector2(0,0))	
 	pass
 
@@ -24,6 +24,7 @@ func computecontact(val):
 
 var floorcontacts=0;
 func _on_feet_body_entered(body):
+	print("touching tralala" +str(floorcontacts))
 	if(body==self):
 		return;
 	computecontact(1)
