d4709082474f902fc7ba1783ff8737f3e351e5b5
Lukas-12 <lukas.buchsi@gmail.com>
2023-12-16 16:19:25 +0100

    Baaaaat Sound

--- Enemies/bat.gd
+++ Enemies/bat.gd
@@ -3,7 +3,7 @@ extends Node2D
 @export var speed = 300
 @onready var player = get_tree().get_first_node_in_group("player")
 var triggered = false
-
+var sound_has_played = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	starty=global_position.y
@@ -15,6 +15,9 @@ func _process(delta):
 	counter=counter+0.1;
 	if abs(player.global_position.x - global_position.x) < 1000:
 		triggered = true
+		if !sound_has_played:
+			sound_has_played = true
+			$batSound.play()
 	if triggered:
 		position.x -= speed * delta
 		position.y =sin(counter)*30+starty
