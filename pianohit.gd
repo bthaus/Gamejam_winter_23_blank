8e651ca421cf2547cc01edebf1f222cbf595e9a6
JohannesLadurner <80148992+JohannesLadurner@users.noreply.github.com>
2023-12-17 10:44:26 +0100

    feat: added string drop for piano, fixed spider

--- pianohit.gd
+++ pianohit.gd
@@ -1,14 +1,33 @@
 extends RigidBody2D
 
+@onready var player = get_tree().get_first_node_in_group("player")
+var rand = RandomNumberGenerator.new()
+var stringDropped = false
+var stringMoveSteps = 50
+var skip = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
+	$String.hide()
+	rand.randomize()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if skip:
+		return
+		
+	$String.rotation = -rotation
+	if stringDropped and (player.onehandbroken or player.twohandbroken or player.leftlegbroken or player.rightlegbroken):
+		if stringMoveSteps > 0:
+			$String.show()
+			$String.global_position.y -= 550 * delta
+			stringMoveSteps = stringMoveSteps - 1
+		else:
+			$String.hide()
+			player.repair()
+			skip = true
 	pass
 
 func hit(type):
@@ -20,6 +39,9 @@ func hit(type):
 		$Area2D/PinJoint2D.free()
 	collision_mask=0;
 	$RigidBody2D2.collision_mask=0
+	if rand.randi_range(0, 100) < 50:
+		stringDropped = true
+		
 var hitted=false;
 func _on_area_2d_area_entered(area):
 	if area==$Area2D:
@@ -31,7 +53,6 @@ func _on_area_2d_area_entered(area):
 		return
 	hitted=true;
 	if(area.get_parent().has_method("hit")):
-		
 		area.get_parent().hit("piano")
 	pass # Replace with function body.
 
