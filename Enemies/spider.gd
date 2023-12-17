986f2d70d6fd3ddd6b5524b0e5260cdd0fcd1be4
JohannesLadurner <80148992+JohannesLadurner@users.noreply.github.com>
2023-12-17 15:00:31 +0100

    Update spider.gd

--- Enemies/spider.gd
+++ Enemies/spider.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @onready var player = get_tree().get_first_node_in_group("player")
 var triggered = false
-var speed = 200
+var speed = 300
 var deadSpeed = 500
 var goDown = true
 var hitSomething = false
@@ -42,7 +42,7 @@ func _process(delta):
 				skip = true
 		return
 		
-	if abs(player.global_position.x - global_position.x) < 50:
+	if abs(player.global_position.x - global_position.x) < 75:
 		triggered = true
 		if !sound_has_played:
 			sound_has_played = true
