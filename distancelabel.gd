1fa1b39e10774de92ae3f6f7b3a58d76658008f6
JohannesLadurner <80148992+JohannesLadurner@users.noreply.github.com>
2023-12-17 12:40:33 +0100

    feat: added distance label

---
+++ distancelabel.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+var moveIn = true
+var labelText = ""
+var moveSpeed = 100
+var moveDistance = 200
+var startx
+var starty
+var counter = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Label.text = labelText
+	startx = position.x
+	starty = position.y
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if $ShowTimer.time_left > 0:
+		return
+	counter += 0.1
+	if moveIn:
+		position.x -= moveSpeed * delta;
+		position.y = sin(counter)*5+starty
+	else:
+		position.x += moveSpeed * delta;
+		position.y = sin(counter)*5+starty
+	if moveIn and startx - position.x >= moveDistance:
+		$ShowTimer.start()
+	if !moveIn and position.x >= startx:
+		queue_free()
+
+func _on_show_timer_timeout():
+	moveIn = false
+	
