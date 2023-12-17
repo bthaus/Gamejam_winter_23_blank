2fd9ba98b6767ad083f332bb89e07a8a9584ac4a
Lukas-12 <lukas.buchsi@gmail.com>
2023-12-17 11:59:14 +0100

    add spawn probability

--- Enemies/trap.gd
+++ Enemies/trap.gd
@@ -2,8 +2,16 @@ extends Node2D
 
 @onready var player = get_tree().get_first_node_in_group("player")
 var triggered = false
+var rand = RandomNumberGenerator.new() 
+var base = 20
+var meter = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	rand.randomize()
+	meter = get_parent().get_parent().distance
+	if  rand.randi_range(0, 100) > base + meter/10:
+		queue_free()
 	pass # Replace with function body.
 
 
